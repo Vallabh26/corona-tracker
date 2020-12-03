e1aa3b7ee9e6667c5cfc6754a2654e0eb0604454
Vallabh Saklani <vallabhsaklani@gmail.com>
2020-12-03 15:39:58 +0530

    Updates

---
+++ precache-manifest.18b55daae752232a254b4ed2e08199a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d596649fec70acfc0d0ad7f79a95ca9b",
+    "url": "/corona-tracker/index.html"
+  },
+  {
+    "revision": "e46b97a502f97ccb50d8",
+    "url": "/corona-tracker/static/css/main.16bc769e.chunk.css"
+  },
+  {
+    "revision": "eb4de1616e085cd039ca",
+    "url": "/corona-tracker/static/js/2.5a348749.chunk.js"
+  },
+  {
+    "revision": "98e7206d2472a9ba0c2c6593c9913f5f",
+    "url": "/corona-tracker/static/js/2.5a348749.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e46b97a502f97ccb50d8",
+    "url": "/corona-tracker/static/js/main.84d6c2ce.chunk.js"
+  },
+  {
+    "revision": "91359a7992344710d3f3",
+    "url": "/corona-tracker/static/js/runtime-main.b32a8805.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/corona-tracker/static/media/covid.d7265326.png"
+  }
+]);
